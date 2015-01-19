e458b42862e07df88bc7fd4ae21adba3f6bbfa3f
Colin Bate <1060912+colinbate@users.noreply.github.com>
2015-01-19 15:16:38 -0400

    Initial development

---
+++ screen.js
@@ -0,0 +1,102 @@
+define(['lib/datetime-arrays', 'require', './admin'], function (dta, require) {
+  'use strict';
+  var plugin = require('./admin'),
+      scheduler = plugin.config.scheduler,
+      getJsDate = function (dateArray, time) {
+        var hours, mins, secs, ret, timeOk = dta.time.isValid(time);
+        if (!dta.dateArray.isValid(dateArray)) {
+          return null;
+        }
+        hours = timeOk && time[0] || 0;
+        mins = timeOk && time[1] || 0;
+        secs = timeOk && time[2] || 0;
+        ret = new Date(dateArray[0], dateArray[1] - 1, dateArray[2], hours, mins, secs);
+        return ret;
+      },
+      pad = function (v) {
+        return v < 10 ? '0' + v : v;
+      },
+      getDuration = function (ms) {
+        var totalseconds = Math.floor(ms / 1000);
+        var seconds = totalseconds % 60;
+        var minutes = Math.floor((totalseconds % 3600) / 60);
+        var hours = Math.floor((totalseconds % 86400) / 3600);
+        var days = Math.floor(totalseconds / 86400);
+        return days + ':' + pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+      },
+      getTimeLeft = function (data) {
+        var parsedTime, parsedDate, now, then;
+        if (!data.parsedTarget) {
+          if (!data.targetDate) {
+            return 'Invalid Target Date';
+          }
+          parsedDate = dta.dateArray.fromString(data.targetDate);
+          parsedTime = data.targetTime ? dta.time.fromString(data.targetTime) : [0, 0, 0];
+          data.parsedTarget = getJsDate(parsedDate, parsedTime);
+          if (!data.parsedTarget) {
+            return 'Invalid Target Date';
+          }
+        }
+        then = data.parsedTarget.getTime();
+        now = (new Date()).getTime();
+        //console.log(then, now, then - now);
+        if (now >= then) {
+          return 'none';
+        }
+        return getDuration(then - now);
+      },
+      updateTimeLeft = function ($scr, str) {
+        var a;
+        if (str !== 'none') {
+          a = Array.prototype.slice.call(str);
+          $scr.find('.time-left').html('<span>' + a.join('</span><span>') + '</span>');
+        } else {
+          $scr.find('.time-left,.static-text').hide();
+        }
+      },
+      localScreen = function () {
+        var self = this,
+            viewData;
+        if (!window.dta) {
+          window.dta = dta;
+        }
+        return {
+          getViewData: function () {
+            var timeLeft = getTimeLeft(self.props.data),
+                eventName = self.props.data.eventName;
+            viewData = {eventName: eventName, timeLeft: Array.prototype.slice.call(timeLeft), notElapsed: timeLeft !== 'none'};
+            return viewData;
+          },
+          preShow: function () {
+            var newTimeLeft = getTimeLeft(self.props.data);
+            updateTimeLeft(self.$screen, newTimeLeft);
+            self.maximizeTextSize();
+          },
+          postShow: function () {
+            var timer;
+            if (!self.animating) {
+              self.animating = true;
+              timer = setInterval(function () {
+                var newTimeLeft;
+                if (!self.$screen || !self.$screen.is(':visible')) {
+                  self.animating = false;
+                  clearInterval(timer);
+                  return;
+                }
+                newTimeLeft = getTimeLeft(self.props.data);
+                updateTimeLeft(self.$screen, newTimeLeft);
+              }, 1000);
+            }
+          },
+          shouldBeShown: function () {
+            var daters = self.props.data,
+                now = (new Date()).getTime();
+
+            return scheduler.isActive(daters, now);
+          }
+        };
+      };
+
+  localScreen.config = plugin.config;
+  return localScreen;
+});
